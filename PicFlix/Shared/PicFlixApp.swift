71a88fd3e58e41d6c26f82a58f3238101372e791
Harshita Saxena <hsaxena500@gmail.com>
2022-12-19 13:05:28 +0530

    Initial commit

---
+++ PicFlix/Shared/PicFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicFlixApp.swift
+//  Shared
+//
+//  Created by rahul anand on 19/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PicFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
